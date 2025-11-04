95eb9a4b00f0d450abf9092044fa0545ac58b77e
Hector <hector14052005@gmail.com>
2025-11-04 20:45:35 +0100

    Hola

--- EditText/app/src/main/java/com/example/edittext/MainActivity.java
+++ EditText/app/src/main/java/com/example/edittext/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.edittext;
 
+import android.media.Rating;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ProgressBar;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,9 +39,80 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        ProgressBar pb = findViewById(R.id.progressBarIndeterminate3);
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            int v = 0;
+            @Override
+            public void run() {
+                if (v<=100){
+                    pb.setProgress(v+=5);
+                    new Handler(Looper.getMainLooper()).postDelayed(this, 200);
+                }
+            }
+        }, 200);
+
         String [] opciones = {"Opcion1", "Opcion2", "Opcion3", "Opcion4", "Opcion5"};
-        AutoCompleteTextView textoLeido = findViewById(R.id.miTexto);
         ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,opciones);
-        textoLeido.setAdapter(adaptador);
+
+        setContentView(R.layout.activity_main);
+        Spinner miSpinner = (Spinner) findViewById(R.id.miSpinner);
+        String[] valores = {"Star Wars", "Reservoir Dogs", "El Club de la lucha", "Snach: Cerdos y Diamantes"};
+        miSpinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, valores));
+
+        miSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                System.out.println("Has seleccionado el valor:" + parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                System.out.println("Has seleccionado nada");
+            }
+        });
+
+        Switch switch1 = (Switch) findViewById(R.id.switch1);
+        TextView textView = (TextView) findViewById(R.id.textview);
+
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    textView.setText("Estado: Encendido");
+                } else {
+                    textView.setText("Estado: Apagado");
+                }
+            }
+        });
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setText("Valor: " + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                //
+            }
+        });
+
+        RatingBar ratingBar = findViewById(R.id.ratingBar);
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                textView.setText("Calificación: "+rating);
+            }
+        });
+
     }
+
 }
